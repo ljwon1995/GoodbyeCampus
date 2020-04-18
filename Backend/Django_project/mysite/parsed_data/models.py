77062b96ea9eceb4141b3d34ba85800915446705
ghksrl6582 <40080899+ghksrl6582@users.noreply.github.com>
2020-04-18 21:41:49 +0900

    subject DB

--- Backend/Django_project/mysite/parsed_data/models.py
+++ Backend/Django_project/mysite/parsed_data/models.py
@@ -8,3 +8,16 @@ class BlogData(models.Model):
 
     def __str__(self):
     	return self.title
+
+class Subject(models.Model):
+    year = models.CharField(max_length=10)
+    category = models.CharField(max_length=10)
+    code = models.CharField(max_length=10)
+    title = models.CharField(max_length=30)
+    credit = models.CharField(max_length=10)
+    etc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.year+' '+self.code+' '+self.title
+
+
