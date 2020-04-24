3b9af7f3adbad4a0761f0ae64ccb4f99163b7e89
HyunWooParkKEIG <yey6687@daum.net>
2020-04-24 23:28:30 +0900

    reupload backend

--- Backend/Django_project/mysite/mysite/wsgi.py
+++ Backend/Django_project/mysite/mysite/wsgi.py
@@ -8,14 +8,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-sys.path.append('/home/ubuntu/GoodbyeCampus/GoodbyeCampus/Backend/Django_project/mysite/elections')
-sys.path.append('/home/ubuntu/django/myprojectenv/lib/python3.6/site-packages')
-
-path = os.path.abspath(__file__+'/../..')
-if path not in sys.path:
-    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
