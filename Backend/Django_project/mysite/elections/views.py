2089870438ff5a601703c930c8982cb2ee28e2a5
Ubuntu <ubuntu@ip-172-31-37-229.us-east-2.compute.internal>
2020-04-25 13:11:15 +0000

    Detect first login and deal with it

--- Backend/Django_project/mysite/elections/views.py
+++ Backend/Django_project/mysite/elections/views.py
@@ -13,37 +13,60 @@ def index(request):
 
 def api(request, message):
 
+        HOST = 'ec2-3-21-126-101.us-east-2.compute.amazonaws.com'
+        PORT = 1024
+
+        if(message == 'start'):
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.bind(('', 0))
+                client_socket.connect((HOST, PORT))
+
+                name = 'Jaewon'
+                msg = ":build Goodcam reset"
+                bot = "HARRY"
+
+                data = name + chr(0) + bot + chr(0) + msg + chr(0);
+                client_socket.send(data.encode())
+                data = client_socket.recv(50000).decode()
+
+                client_socket.close()
+
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client_socket.bind(('', 0))
+                client_socket.connect((HOST, PORT))
+
+                msg = ""
+
+                data = name + chr(0) + bot + chr(0) + msg + chr(0);
+                client_socket.send(data.encode());
+
+                data = client_socket.recv(10000).decode();
+
+
+
+
+                return JsonResponse({
+                        'message': 1,
+                        'content':  data
+                        })
+
+
+
+            
+
         print('user message = ', message)
 
 	#to the chatbot(message)
 	#result = message.upper()
 
 
-        HOST = 'ec2-3-21-126-101.us-east-2.compute.amazonaws.com'
-        PORT = 1024
-
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.bind(('', 0))
-        client_socket.connect((HOST, PORT))
-
-
-
-        name = 'Jaewon'
-        msg = ":build GOODCAM reset"
-        bot = "Harry"
-
-        data = name + chr(0) + bot + chr(0) + msg + chr(0);
-
-        client_socket.send(data.encode());
-
-
-        
-        data = client_socket.recv(10000);
-        print('bot : ', data.decode());
 
 
 
         print("Conversation Started");
+
+        name = 'Jaewon'
+        bot = "HARRY"
 
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
