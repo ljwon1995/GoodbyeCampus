2b01c8180b84096e734955b8c3f274813f2a9b14
ghksrl6582 <40080899+ghksrl6582@users.noreply.github.com>
2020-04-26 04:22:35 +0900

    revise crawler

--- Backend/Django_project/mysite/parsed_data/models.py
+++ Backend/Django_project/mysite/parsed_data/models.py
@@ -8,3 +8,17 @@ class BlogData(models.Model):
 
     def __str__(self):
     	return self.title
+
+class CourseList(models.Model):
+    year = models.CharField(max_length=10)
+    category = models.CharField(max_length=10)
+    code = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
+    credit = models.CharField(max_length=5)
+    etc = models.CharField(max_length=500)
+
+    def __str__(self):
+    	return self.year, self.code, self.title
+
+
+
